75ab96f8f02a450440bf504c7f4fa9bbca1a256f
Jaden Weiss <jadr2ddude@gmail.com>
2017-07-02 13:29:46 -0400

    improve error handling and allow rebuilding particular packages

--- repobuild.sh
+++ repobuild.sh
@@ -1,8 +1,11 @@
+if [ "$#" -lt 1 ]; then
+	echo "script requires at least one argument"
+	exit 1
+fi
 if [ ! -d packages-main ]; then
 	git clone https://github.com/panux/packages-main.git || { echo "Failed to git clone package repo"; exit 1; }
 fi
-if [ ! -d repo/$1/pkgs ]
-then
+if [ ! -d repo/$1/pkgs ]; then
 	mkdir -p repo/$1/pkgs || { echo "Failed to create repo directory"; exit 1; }
 fi
 git -C packages-main pull || { echo "Failed to pull package repo"; exit 1; }
@@ -14,9 +17,15 @@ function cleanup() {
 	rm -f $LST
 }
 trap cleanup EXIT
-for i in packages-main/*.pkgen; do
-	docker run --rm -v $(realpath packages-main):/build -v $TMPDIR:/out panux/package-builder /build/$(basename $i) /out || { echo "Failed to build package $i"; exit 1; }
-done
+if [ "$#" -lt 2 ]; then
+	for i in packages-main/*.pkgen; do
+		docker run --rm -v $(realpath packages-main):/build -v $TMPDIR:/out panux/package-builder /build/$(basename $i) /out || { echo "Failed to build package $i"; exit 1; }
+	done
+else
+	for i in "${@:2}"; do
+		docker run --rm -v $(realpath packages-main):/build -v $TMPDIR:/out panux/package-builder /build/$(basename $i) /out || { echo "Failed to build package $i"; exit 1; }
+	done
+fi
 ls $TMPDIR
 for i in $TMPDIR/*.tar.xz; do
 	cmp --silent "$i" repo/$1/pkgs/$(basename -s .tar.xz "$i") && {
