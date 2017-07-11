19eb2f0b1a1904f93ba03efad4011bae0487040a
Jaden Weiss <jadr2ddude@gmail.com>
2017-07-11 19:53:43 -0400

    update repobuild.sh for multiple architecture support

--- repobuild.sh
+++ repobuild.sh
@@ -1,15 +1,15 @@
-if [ "$#" -lt 1 ]; then
-	echo "script requires at least one argument"
+if [ "$#" -lt 2 ]; then
+	echo "script requires at least two arguments"
 	exit 1
 fi
 if [ ! -d packages-main ]; then
 	git clone https://github.com/panux/packages-main.git || { echo "Failed to git clone package repo"; exit 1; }
 fi
 if [ ! -d repo/$1/pkgs ]; then
-	mkdir -p repo/$1/pkgs || { echo "Failed to create repo directory"; exit 1; }
+	mkdir -p repo/$1/$2/pkgs || { echo "Failed to create repo directory"; exit 1; }
 fi
 git -C packages-main pull || { echo "Failed to pull package repo"; exit 1; }
-docker pull panux/package-builder || { echo "Failed to pull package-builder image"; exit 1; }
+docker pull panux/package-builder:$2 || { echo "Failed to pull package-builder image"; exit 1; }
 TMPDIR=$(mktemp -d)
 LST=$(mktemp)
 function cleanup() {
@@ -18,18 +18,19 @@ function cleanup() {
 }
 trap cleanup EXIT
 if [ "$#" -lt 2 ]; then
-	make -C packages-main DEST=$TMPDIR all || { echo "Failed to build package(s)"; exit 1; }
+	make -C packages-main ARCH=$2 DEST=$TMPDIR all || { echo "Failed to build package(s)"; exit 1; }
 else
-	make -C packages-main DEST=$TMPDIR "${@:2}" || { echo "Failed to build package(s)"; exit 1; }
+	make -C packages-main ARCH=$2 DEST=$TMPDIR "${@:3}" || { echo "Failed to build package(s)"; exit 1; }
 fi
 ls $TMPDIR
 for i in $TMPDIR/*.tar.xz; do
-	cmp --silent "$i" repo/$1/pkgs/$(basename -s .tar.xz "$i") && {
+	cmp --silent "$i" repo/$1/$2/pkgs/$(basename -s .tar.xz "$i") && {
 		rm "$i" || { echo "Removal error"; exit 1; }
 	} || {
 		gpg --output $TMPDIR/$(basename -s .tar.xz "$i").sig --detach-sig "$i" || { echo "Signature failure"; exit 1; }
+		gpgv $TMPDIR/$(basename -s .tar.xz "$i").sig $i || { echo "Bad signature"; exit 1; }
 	}
 done
 for i in $TMPDIR/*; do
-	mv $i repo/$1/pkgs/$(basename "$i") || { echo "Move error"; exit 1; }
+	mv $i repo/$1/$2/pkgs/$(basename "$i") || { echo "Move error"; exit 1; }
 done
