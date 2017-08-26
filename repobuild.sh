a091992e84c5d70a36e0366558a608e6e53b32e9
Jaden Weiss <jadr2ddude@gmail.com>
2017-08-26 10:27:23 -0400

    random updates

--- repobuild.sh
+++ repobuild.sh
@@ -6,6 +6,7 @@ if [ -d packages-main ]; then
 	rm -rf packages-main
 fi
 git clone -b $1 https://github.com/panux/packages-main.git || { echo "Failed to git clone package repo"; exit 1; }
+echo clone done
 if [ ! -d repo/$1/$2/pkgs ]; then
 	mkdir -p repo/$1/$2/pkgs || { echo "Failed to create repo directory"; exit 1; }
 fi
@@ -28,6 +29,13 @@ for i in $TMPDIR/*.tar.xz; do
 	cmp --silent "$i" repo/$1/$2/pkgs/$(basename -s .tar.xz "$i") && {
 		rm "$i" || { echo "Removal error"; exit 1; }
 	} || {
+		cp $i $TMPDIR/$(basename -s .tar.xz "$i").tar
+		tar -xf $TMPDIR/$(basename -s .tar.xz "$i").tar -C $TMPDIR ./.pkginfo
+		mv $TMPDIR/.pkginfo $TMPDIR/$(basename -s .tar.xz "$i").pkginfo
+		gzip $TMPDIR/$(basename -s .tar.xz "$i").tar
+		echo SHA256SUM="\""$(sha256sum "$i" | cut -d' ' -f1)"\"" >> $TMPDIR/$(basename -s .tar.xz "$i").pkginfo
+		gpg --output $TMPDIR/$(basename -s .tar.xz "$i").pkginfo.sig --sign $TMPDIR/$(basename -s .tar.xz "$i").pkginfo || { echo "Signature failure"; exit 1; }
+		echo | minisign -SHm $TMPDIR/$(basename -s .tar.xz "$i").pkginfo || { echo "minisign signature failure"; exit 1; }
 		gpg --output $TMPDIR/$(basename -s .tar.xz "$i").sig --detach-sig "$i" || { echo "Signature failure"; exit 1; }
 		gpgv $TMPDIR/$(basename -s .tar.xz "$i").sig $i || { echo "Bad signature"; exit 1; }
 	}
